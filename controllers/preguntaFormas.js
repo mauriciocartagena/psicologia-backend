17850bb6d281057c982883be6d0687e80b414c09
Mauricio Cartagena <mc973220@gmail.com>
2021-11-07 14:52:33 -0400

    Se mejoro el rendimiento de la app

--- controllers/preguntaFormas.js
+++ controllers/preguntaFormas.js
@@ -1,141 +1,111 @@
+const { response } = require("express");
+const { PreguntasFormas, TestFormas } = require("../database/config");
 
-const { response } = require('express');
-const { PreguntasFormas, TestFormas } = require('../database/config');
+const mostrarPreguntaForma = async (req, res = response) => {
+  const limit = parseInt(req.query.limit); // Asegúrate de parsear el límite a número
+  const skip = parseInt(req.query.skip);
 
-const mostrarPreguntaForma = async ( req, res = response ) => { 
+  const preguntaFormas = await PreguntasFormas.findAll({
+    limit: limit,
+    offset: skip,
+    include: [{ model: TestFormas, as: "test_formas", attributes: ["nombre"] }],
+  });
 
-    const limit = parseInt( req.query.limit ); // Asegúrate de parsear el límite a número
-    const skip  = parseInt( req.query.skip );
-
-    const preguntaFormas = await PreguntasFormas.findAll({
-        limit: limit,
-        offset: skip,
-        include: [
-            { model: TestFormas, as: 'test_formas',
-              attributes:['nombre']
-            }
-        ]
-    });
- 
-     res.status( 200 ).json({
-         ok:true,
-         msg: 'lista de preguntas formas',
-         preguntaFormas
-     });
+  res.status(200).json({
+    ok: true,
+    msg: "lista de preguntas formas",
+    preguntaFormas,
+  });
 };
-const mostrarPreguntaOneForma = async ( req, res = response ) => { 
+const mostrarPreguntaOneForma = async (req, res = response) => {
+  const { id_pregunta } = req.body;
 
-    const { id_pregunta  } = req.body;
-
-    const preguntaFormas = await PreguntasFormas.findOne({
-        where:{ id_pregunta: id_pregunta},
-            include: [
-                { model: TestFormas, as: 'test_formas',
-                attributes:['nombre']
-                }
-            ]
+  const preguntaFormas = await PreguntasFormas.findOne({
+    where: { id_pregunta: id_pregunta },
+    include: [{ model: TestFormas, as: "test_formas", attributes: ["nombre"] }],
+  });
+  try {
+    res.status(200).json({
+      ok: true,
+      msg: "lista de preguntas formas",
+      preguntaFormas,
     });
-     try {
-        res.status( 200 ).json({
-            ok:true,
-            msg: 'lista de preguntas formas',
-            preguntaFormas
-        });   
-         
-     } catch (error) {
-         console.log( error )
-         res.status( 400 ).json({
-             ok:false,
-             msg:'Por favor hable con el Administrador'
-         });
-     }
- 
-
-
-
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      ok: false,
+      msg: "Por favor hable con el Administrador",
+    });
+  }
 };
 
-const crearPreguntaForma = async ( req, res = response ) => { 
-    
-    const name = await PreguntasFormas.findOne({
-        where:{ nombre : req.body.nombre }
-    });
-    
-    
-    try {
-            
-        if ( name ) {
-            return res.status( 400 ).json({
-                ok:false,
-                msg: 'La pregunta ya existe'
-           });
-        }
-        await PreguntasFormas.create( req.body );
-            
-        res.status( 200 ).json({
-            ok:true,
-            msg: 'register'
-        });
+const crearPreguntaForma = async (req, res = response) => {
+  const name = await PreguntasFormas.findOne({
+    where: { nombre: req.body.nombre },
+  });
 
-    } catch (error) {
-        res.status(201).json({
-          ok:true,
-          msg:'Por favor hable con el Administrador',
+  try {
+    if (name) {
+      return res.status(400).json({
+        ok: false,
+        msg: "La pregunta ya existe",
       });
     }
+    await PreguntasFormas.create(req.body);
 
+    res.status(200).json({
+      ok: true,
+      msg: "register",
+    });
+  } catch (error) {
+    res.status(201).json({
+      ok: true,
+      msg: "Por favor hable con el Administrador",
+    });
+  }
 };
 
-const updatePreguntaForma = async ( req, res = response ) => {
-
-    await PreguntasFormas.update( req.body, { 
-
-        where:{ id_pregunta : req.body.id_pregunta }
-
-    }).then( () => {
-
-        res.status( 200 ).json({
-
-            ok:true,
-            msg:'update'
-
-        });
-
-    }).catch( () => {
-
-        res.status( 400 ).json({
-
-            ok:false,
-            msg:'error update'
-
-        });
-
+const updatePreguntaForma = async (req, res = response) => {
+  await PreguntasFormas.update(req.body, {
+    where: { id_pregunta: req.body.id_pregunta },
+  })
+    .then(() => {
+      res.status(200).json({
+        ok: true,
+        msg: "update",
+      });
+    })
+    .catch(() => {
+      res.status(400).json({
+        ok: false,
+        msg: "error update",
+      });
     });
+};
 
-}
-
-const deletePreguntaForma = async ( req, res = response ) => {
-
-    await PreguntasFormas.destroy({
-        where:{ id_pregunta: req.body.id_pregunta  },
-    }).then( () => {
-        res.status(200).json({
-            ok:true,
-            msg:'delete'
-        });
-    }).catch( (error)=> {
-        console.log( error )
-        res.status(400).json({
-            ok:false,
-            msg:'error delete'
-        });
+const deletePreguntaForma = async (req, res = response) => {
+  await PreguntasFormas.destroy({
+    where: { id_pregunta: req.body.id_pregunta },
+  })
+    .then(() => {
+      res.status(200).json({
+        ok: true,
+        msg: "delete",
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json({
+        ok: false,
+        msg: "error delete",
+      });
     });
-}
+};
 
 module.exports = {
-    mostrarPreguntaForma,
-    crearPreguntaForma,
-    updatePreguntaForma,
-    deletePreguntaForma,
-    mostrarPreguntaOneForma
-}
+  mostrarPreguntaForma,
+  crearPreguntaForma,
+  updatePreguntaForma,
+  deletePreguntaForma,
+  mostrarPreguntaOneForma,
+};
